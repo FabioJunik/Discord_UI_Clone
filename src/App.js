682ffad7cbe8c3cfc95bdda2716efdbb37d7f07b
Fábio Junik <fabiojunik@gmail.com>
2022-04-15 19:51:48 +0100

    Extrutura inicial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+      <>
+        <GlobalStyles/>
+        <h1>Fábio Junik</h1>
+      </>
+    );
+}
+
+export default App;
---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Layout
