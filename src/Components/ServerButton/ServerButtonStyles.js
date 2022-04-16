a487c6f85571e703f7912970c37af1ca80cabdc3
Fábio Junik <fabiojunik@gmail.com>
2022-04-16 07:50:28 +0100

    Notificação e mentions criadas

--- src/Components/ServerButton/ServerButtonStyles.js
+++ src/Components/ServerButton/ServerButtonStyles.js
@@ -17,6 +17,36 @@ export const Button = styled.button`
         height: 24px;
     }
 
+    &::before{
+        content:'';
+        position: absolute;
+        width: 9px;
+        height: 9px;
+        left: -17px;
+        top: calc(50% - 4.5px);
+        background-color: var(--white);
+        border-radius: 50%;
+        display: ${({hasNotifications})=> hasNotifications?'inline':'none'};
+    }
+
+    &::after{
+        content: '${({mentions})=> mentions&&mentions }';
+        display: ${({mentions}) => mentions>0?'inline':'none'};
+        position: absolute;
+        background-color: var(--notification);
+        width:auto;
+        height: 16px;
+        padding: 0 4px;
+        bottom:-4px;
+        right:-4px;
+        border: 4px solid var(--quaternary);
+        border-radius: 12px;
+        font-size: 13px;
+        text-align: center;
+        font-weight: bold;
+        color:var(--white);
+    }
+
     transition: border-radius .2s, background-color .2s;
 
     &.active ,
