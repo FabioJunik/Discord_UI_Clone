a3f8ed35a6eedec97d1fff642b2f1f5b266241de
Fábio Junik <fabiojunik@gmail.com>
2022-04-16 10:53:50 +0100

    Area Server Name criada

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import LayoutStyles from './LayoutStyles';
 import ServerList from '../ServerList/ServerList';
+import ServerName from '../ServerName/ServerName';
 
 const Layout = () => {
 
 return (
     <LayoutStyles>
         <ServerList/>
+        <ServerName/>
     </LayoutStyles>
 )
 }
---
+++ src/Components/ServerName/ServerName.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Container, Title, ExpandIcon} from './ServerNameStyles'
+
+const ServerName = () => {
+  return (
+    <Container>
+        <Title>Servidor do Fábio</Title>
+        <ExpandIcon/>
+    </Container>
+  )
+}
+
+export default ServerName
